0bbce269cd66433c25290ac92a50bb89e908299a
ivan <example@gmail.com>
2020-11-21 17:25:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+'use strict';
+var a = 8;
+console.log(a);
+/*console.log(leftBorderWidth);
+var leftBorderWidth = 1;
+
+// Типы данных(их 7)
+
+var number = 5;
+var string = "hello";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+
+console.log(4/0);
+console.log("string"*9);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "john",
+    age: "25",
+    isMarried: false
+}
+
+console.log(person["name"]);
+*/
+let arr = ['plum.png','orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+//alert("hello world");
+
+//let answer = confirm("Are you here?");
+
+//console.log(answer);
+// Типы данных содержат "Простые типы" и "Объекты"
+
+//let answer = prompt("Есть ли вам 18?", "Да");
+/*
+console.log(typeof(null));
+
+console.log("arr" + "- object");
+console.log(4 + +"- object");
+*/
+
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
